62ce6270ebb6b5d05787d2202fa1193deffca077
PerpetualOgechi <perpetualogechi34@gmail.com>
2024-08-22 09:08:30 -0700

    categories

--- components/miniCategory/MiniCategory.jsx
+++ components/miniCategory/MiniCategory.jsx
@@ -4,35 +4,43 @@ const MiniCategory = () => {
     const categories = [
         {
             name: "Design",
-            desc: "200+ jobs available"
+            desc: "200+ jobs available",
+            image: "/design.svg"
         },
         {
             name: "Sales",
-            desc: "200+ jobs available"
+            desc: "200+ jobs available",
+            image: "/sales.svg"
         },
         {
             name: "Marketing",
-            desc: "200+ jobs available"
+            desc: "200+ jobs available",
+            image: "/marketing.svg"
         },
         {
             name: "Finance",
-            desc: "200+ jobs available"
+            desc: "200+ jobs available",
+            image: "/finance.svg"
         },
         {
             name: "Tecnology",
-            desc: "200+ jobs available"
+            desc: "200+ jobs available",
+            image: "/technology.svg"
         },
         {
             name: "Engineering",
-            desc: "200+ jobs available"
+            desc: "200+ jobs available",
+            image: "/engineering.svg"
         },
         {
             name: "Business",
-            desc: "200+ jobs available"
+            desc: "200+ jobs available",
+            image: "/business.svg"
         },
         {
             name: "Human Resource",
-            desc: "200+ jobs available"
+            desc: "200+ jobs available",
+            image: "/resources.svg"
         }
     ]
     return (
@@ -48,7 +56,7 @@ const MiniCategory = () => {
             </div>
 
             <div>
-                <div className='px-8 py-8 pb-36 relative'
+                <div className='px-8 py-8 pb-36 '
                     style={{
                         display: "grid",
                         gridTemplateColumns: "repeat(auto-fit,minmax(250px,1fr))",
@@ -60,10 +68,11 @@ const MiniCategory = () => {
                 >
                     {
                         categories.map((category, index) => {
-                            const { name, desc } = category
+                            const { name, desc, image } = category
                             return (
-                                <div key={index} className='bg-white rounded-xl px-5 py-5  flex flex-col gap-5 justify-center h-[250px]'>
-                                    <h1 className='text-[18px] mt-10 font-medium text-[#3b3b3b]'>{name}</h1>
+                                <div key={index} className='bg-white rounded-xl px-5 py-10 flex flex-col gap-5 justify-center '>
+                                    <img src={image} alt="" className='w-[50px] h-[50px]' />
+                                    <h1 className='text-[18px] font-medium text-[#3b3b3b]'>{name}</h1>
                                     <div className='flex justify-between'>
                                         <p>{desc}</p>
                                         <img src="Arrow 1.svg" alt="" className='w-[35px]' />
@@ -72,21 +81,22 @@ const MiniCategory = () => {
                             )
                         })
                     }
-                    <div className='bg-white w-full justify-between items-center rounded-md absolute  py-10 pl-10  bottom-[-100px] sb:flex pr-36'>
-                        <div>
-                            <h1 className='font-medium text-center text-[30px] sb:text-[44px]'>Get <span className='text-[#10B981]'>Job Alart</span> ?</h1>
-                        </div>
 
-                        <div className='gap-5 md:flex '>
-                            <div className='border-4 rounded-3xl border-[#10B981] p-5 flex '>
-                                <input type="text" placeholder='Enter Your Email' className='w-full outline-none text-[16px]' />
-
-                            </div>
-                            <button className='bg-[#10B981]  px-8 text-[20px] text-white rounded-2xl md:ml-[-50px]'>Search</button>
-                        </div>
-
-
+                </div>
+                <div className='bg-white w-full justify-between items-center rounded-md  py-10 pl-10  md:flex pr-36'>
+                    <div>
+                        <h1 className='font-medium text-center text-[30px] sb:text-[44px]'>Get <span className='text-[#10B981]'>Job Alart</span> ?</h1>
                     </div>
+
+                    <div className='gap-5 md:flex '>
+                        <div className='border-4 rounded-3xl border-[#10B981] p-5 flex '>
+                            <input type="text" placeholder='Enter Your Email' className='w-full outline-none text-[16px]' />
+
+                        </div>
+                        <button className='bg-[#10B981]  px-8 text-[20px] text-white rounded-2xl md:ml-[-50px]'>Search</button>
+                    </div>
+
+
                 </div>
             </div>
         </div>
