d0c81b70271416ff0a5ecb1aec301a47f2182694
PerpetualOgechi <perpetualogechi34@gmail.com>
2024-08-22 06:23:38 -0700

    jobDetails

--- app/jobs/[id]/page.js
+++ app/jobs/[id]/page.js
@@ -1,13 +1,10 @@
 "use client"
-import JobDetails from '@/components/jobDetails/jobDetails'
+import JobDetails from '@/components/jobDetails/JobDetails'
 import Navbar from '@/components/navbar/Navbar'
 import React from 'react'
-import { useParams } from 'next/navigation';
+
 
 const page = () => {
-  const { id } = useParams()
-  // const router = useRouter();
-  // const { id } = router.query;
   return (
     <div>
       <Navbar />
